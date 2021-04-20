e4b54f89c50263b1fb553c5246f5e0772a5168fb
Rajat Sharda <>
2021-04-20 22:25:45 +0530

    Added env support for Django secret key.

--- daksha/settings.py
+++ daksha/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gj%7t91&c$*q&+fkx-wuug^i$0b@@lk26vd4txdq=)%_319hyy'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'gj%7t91&c$*q&+fkx-wuug^i$0b@@lk26vd4txdq=)%_319hyy')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
