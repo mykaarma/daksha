39ae9149589b144734915a14e7fe32e25b44d6d1
Rajat Sharda <>
2021-04-22 17:41:00 +0530

    Turn off debug mode

--- daksha/settings.py
+++ daksha/settings.py
@@ -39,7 +39,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'gj%7t91&c$*q&+fkx-wuug^i$0b@@lk26vd4txdq=)%_319hyy')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
   '127.0.0.1'
