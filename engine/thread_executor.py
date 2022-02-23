1e75a62778f45d6cad0ffc6f549706243859d8b9
hriddhighosh4 <hriddhi.ghosh@mykaarma.com>
2022-02-23 18:59:39 +0530

    Test Executore change

--- engine/thread_executor.py
+++ engine/thread_executor.py
@@ -1,10 +1,12 @@
 import concurrent
 from concurrent.futures.thread import ThreadPoolExecutor
 
-from engine.executor import execute_test, TestExecutor, json, generate_report
+from engine.executor import execute_test
 from .email_generator import send_report_email
 from .logs import *
 from daksha.settings import STORAGE_PATH, APACHE_URL
+from .models import TestExecutor
+from .testreport_generator import generate_report
 
 
 def thread_executor(test_ymls, initial_variable_dictionary, test_id, email):
