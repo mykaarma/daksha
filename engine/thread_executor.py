ff68d7045daf0ff13a64425d0ef846870718f47d
hriddhighosh4 <69582822+hriddhighosh4@users.noreply.github.com>
2022-03-07 11:20:19 +0530

    Update thread_executor.py

--- engine/thread_executor.py
+++ engine/thread_executor.py
@@ -1,5 +1,20 @@
-from concurrent.futures.thread import ThreadPoolExecutor
+"""
+Daksha
+Copyright (C) 2022 myKaarma.
+opensource@mykaarma.com
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty tof
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
+from concurrent.futures.thread import ThreadPoolExecutor
 from engine.executor import execute_test
 from .email_generator import send_report_email
 from .logs import *
