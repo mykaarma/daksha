e1a2ce9a8b7030d6f306f695a3207b8a641715c6
pulkitsinghall <pulkit.singhal@mykaarma.com>
2025-06-30 19:17:35 +0530

    Update settings.py

--- daksha/settings.py
+++ daksha/settings.py
@@ -29,6 +29,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import yaml
 from reportportal_client import RPClient
+from .logs import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
