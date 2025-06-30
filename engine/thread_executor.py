039c6fcef1bff0640c7eb3638273a37fdce30311
pulkitsinghall <pulkit.singhal@mykaarma.com>
2025-06-30 17:26:10 +0530

    Update thread_executor.py

--- engine/thread_executor.py
+++ engine/thread_executor.py
@@ -65,7 +65,6 @@ def thread_executor(test_ymls, initial_variable_dictionary, test_uuid, email):
     if REPORT_PORTAL_ENABLED != None and REPORT_PORTAL_ENABLED.lower() == "true":
         report_portal_service.finish_launch(item_id = launch_id, end_time = timestamp())
         logger.info(f"Tests finished. Ending launch Daksha_test_{test_uuid} in Report Portal ")
-        report_portal_service.terminate()
         
     logger.info("All threads complete, generating test report")
     generate_report(test_uuid)
