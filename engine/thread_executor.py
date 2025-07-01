5021aa86084c3326e76fd3e6ff41c65852e922c8
pulkitsinghall <pulkit.singhal@mykaarma.com>
2025-07-01 10:43:52 +0530

    Update thread_executor.py

--- engine/thread_executor.py
+++ engine/thread_executor.py
@@ -29,7 +29,7 @@ def thread_executor(test_ymls, initial_variable_dictionary, test_uuid, email):
     testExecutorObjects=[]
     
     if(REPORT_PORTAL_ENABLED != None and REPORT_PORTAL_ENABLED.lower() == "true"):
-        # report_portal_service.start()
+        report_portal_service.start()
         logger.info("User has opted for Test Reports to be displayed on Report Portal")
         launch_id = report_portal_service.start_launch(name = f"Daksha_test_{test_uuid}", mode = 'DEFAULT', start_time = timestamp())
         logger.info(f"Initiating launch in Report Portal with name {test_uuid}")
@@ -63,7 +63,7 @@ def thread_executor(test_ymls, initial_variable_dictionary, test_uuid, email):
         pass
 
     if REPORT_PORTAL_ENABLED != None and REPORT_PORTAL_ENABLED.lower() == "true":
-        report_portal_service.finish_launch(item_id = launch_id, end_time = timestamp())
+        report_portal_service.finish_launch(launch_uuid = launch_id, end_time = timestamp())
         logger.info(f"Tests finished. Ending launch Daksha_test_{test_uuid} in Report Portal with id {launch_id}")
         # report_portal_service.terminate()
         
